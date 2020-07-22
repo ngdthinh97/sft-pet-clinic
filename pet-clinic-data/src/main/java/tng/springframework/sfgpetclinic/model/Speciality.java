9c17a8ec5d155e7424e0e5bc81ff0058c9d7140a
ngdthinh97 <ngdthinh.97@gmail.com>
2020-07-22 15:13:05 +0700

    Added Speciality class and associate to Vet

---
+++ pet-clinic-data/src/main/java/tng/springframework/sfgpetclinic/model/Speciality.java
@@ -0,0 +1,15 @@
+package tng.springframework.sfgpetclinic.model;
+
+public class Speciality {
+	
+	private String description;
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+}
